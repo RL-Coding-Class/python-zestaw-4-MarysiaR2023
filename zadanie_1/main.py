8effdecff57602d2d352554335475bf375391cc6
MarysiaR <maria.1.rostworowska@student.uj.edu.pl>
2025-01-25 19:16:30 +0100

    zad 1 2-nd ver.

--- zadanie_1/main.py
+++ zadanie_1/main.py
@@ -1,11 +1,11 @@
 import time
 import schedule
-from database import create_table
-from flight_data import fetch_flight_data, plot_flight_data
+from zadanie_1.database import create_table
+from zadanie_1.flight_data import fetch_flight_data, plot_flight_data
 
 def main(interval, max_repeats):
     create_table(max_repeats)
- 
+
     # Licznik iteracji
     counter = 0
 
@@ -25,7 +25,7 @@ def main(interval, max_repeats):
     while counter < max_repeats:
         schedule.run_pending()
         time.sleep(1)  # Krótkie opóźnienie zmniejsza obciążenie CPU
-    return None
+
 
 
 if __name__ == '__main__':
